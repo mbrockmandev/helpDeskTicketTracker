327efc52c978d2cd2d558aa58f5a327d2d8ebfa2
Michael Brockman <mbrockmandev@gmail.com>
2023-03-19 09:36:12 -0400

    connect to mongo
    add error middleware
    
    add user controller and routes (not done)

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('server!');
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000;
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+// connect to MongoDB
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Hello!'})
+})
+
+app.use('/api/users/', require('./routes/userRoutes'))
+app.use(errorHandler)
+app.listen(PORT, () => console.log(`server started on ${PORT}`))
