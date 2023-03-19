cf255adf3ac26320d63850abafc6be1968d4aff3
Michael Brockman <mbrockmandev@gmail.com>
2023-03-19 10:14:28 -0400

    add get current user

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+const protectRoute = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // const bearer = req.headers.authorization.startsWith('Bearer'.toLowerCase())
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // get token from header
+      token = req.headers.authorization.split(' ')[1]
+      // verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      // get user from token
+      req.user = await User.findById(decoded.id).select('-password')
+
+      next()
+    } catch (e) {
+      console.log(e)
+      res.status(401)
+      throw new Error('Not authorized.')
+    }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized.')
+  }
+})
+
+module.exports = {protectRoute}
